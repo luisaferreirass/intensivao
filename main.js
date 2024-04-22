fc697917779b5e85078790c5d39c0dd9cbc25035
luisaferreirass <152530880+luisaferreirass@users.noreply.github.com>
2024-04-22 15:31:04 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,80 @@
+// questão 1
+alert("Hello World!")
+
+// questão 2
+let number1 = Math.round(Math.random() * 10)
+let number2 = Math.round(Math.random() * 10)
+
+let result = number1 + number2
+alert(`A soma entre os números é: ${result}`)
+
+// questão 3
+
+let variavel = 18
+
+if (typeof isNumber == 'number') {
+    alert('É um número')
+} else {
+    alert('Não é um número')
+}
+
+// questão 4
+
+let variavel2 = "Luisa"
+
+if (typeof variavel2 == 'string'){
+    alert("É uma string")
+}
+
+else {
+    alert("Não é uma String")
+}
+
+// questão 5
+
+let variavel3 = true
+if (typeof variavel3 == 'boolean'){
+    alert("É um booleano")
+}
+
+else {
+    alert("Não é um boolano")
+}
+
+// questão 6
+let number3 = Math.round(Math.random() * 10)
+let number4 = Math.round(Math.random() * 10)
+
+let result1 = number3 - number4
+alert(`A subtração entre os números é: ${result1}`)
+
+// questão 7
+
+let number5 = Math.round(Math.random() * 10)
+let number6 = Math.round(Math.random() * 10)
+
+let result2 = number5 * number6
+alert(`A multiplicação entre os números é: ${result2}`)
+
+// questão 8
+let numbe7 = Math.round(Math.random() * 10)
+let number8 = Math.round(Math.random() * 10)
+
+let result3 = number7 / number8
+alert(`A divisão entre os números é: ${result3}`)
+
+// questão 9
+let numero = Number(prompt("Dìgite um número"))
+if (numero % 2 == 0){
+    alert("É um número par")
+}
+
+else {alert("Não é um número par")}
+
+// questão 10
+let numero1 = Number(prompt("Dìgite um número"))
+if (numero1 % 2 != 0){
+    alert("É um número ímpar")
+}
+
+else {alert("Não é um número ímpar")}
